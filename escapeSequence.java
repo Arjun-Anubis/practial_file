a9017ebd15633c9c366e84a51ed4048a1da5d963
Arjun-Anubis <anubiz512z@gmail.com>
2022-01-11 01:05:06 +0530

    Added more escape sequence magic

--- escapeSequence.java
+++ escapeSequence.java
@@ -9,9 +9,23 @@ class escapeSequence
 	private static final String Magenta = Escape + "35m";
 	private static final String Cyan = Escape + "36m";
 	private static final String White = Escape + "37m";
+
 	private static final String Clear = Escape + "00m";
+
+	private static final String Bold = Escape + "01m";
+	private static final String Italic = Escape + "03m";
+	private static final String UnderLine = Escape + "04m";
+	private static final String Blink = Escape + "05m";
+	private static final String Encircled = Escape + "51m";
+
 	public static void main (String [] args)
 	{
 		System.out.printf( "%s White %s Red %s Green %s Brown %s Blue %s Magenta %s Cyan %s Black %s\n", White, Red, Green, Brown, Blue, Magenta, Cyan, Black,  Clear );
+		System.out.printf( "%s%s White %s Red %s Green %s Brown %s Blue %s Magenta %s Cyan %s Black %s\n", UnderLine, White, Red, Green, Brown, Blue, Magenta, Cyan, Black,  Clear );
+		System.out.printf( "%s%s White %s Red %s Green %s Brown %s Blue %s Magenta %s Cyan %s Black %s\n", Bold, White, Red, Green, Brown, Blue, Magenta, Cyan, Black,  Clear );
+		System.out.printf( "%s%s White %s Red %s Green %s Brown %s Blue %s Magenta %s Cyan %s Black %s\n", Italic, White, Red, Green, Brown, Blue, Magenta, Cyan, Black,  Clear );
+		System.out.printf( "%s%s White %s Red %s Green %s Brown %s Blue %s Magenta %s Cyan %s Black %s\n", Blink, White, Red, Green, Brown, Blue, Magenta, Cyan, Black,  Clear );
+		System.out.printf( "%s%s White %s Red %s Green %s Brown %s Blue %s Magenta %s Cyan %s Black %s\n", Encircled, White, Red, Green, Brown, Blue, Magenta, Cyan, Black,  Clear );
+
 	}
 }
